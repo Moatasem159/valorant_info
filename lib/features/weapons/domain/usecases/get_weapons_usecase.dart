8b040d4956b51b462788cda22437d0554861840e
moatasem <moatasemmohamed39@gmail.com>
2023-02-17 23:59:48 +0200

    modify weapons usecase

--- lib/features/weapons/domain/usecases/get_weapons_usecase.dart
+++ lib/features/weapons/domain/usecases/get_weapons_usecase.dart
@@ -2,9 +2,9 @@ import 'package:dartz/dartz.dart';
 import 'package:valorant_info/core/error/failure.dart';
 import 'package:valorant_info/features/weapons/domain/entities/weapon.dart';
 import 'package:valorant_info/features/weapons/domain/repositories/weapon_repository.dart';
-class GetAgentsUseCase {
+class GetWeaponsUseCase {
   final WeaponRepository _weaponRepository;
-  GetAgentsUseCase(this._weaponRepository);
+  GetWeaponsUseCase(this._weaponRepository);
   Future<Either<Failure, List<Weapon>>> call() async {
     return await _weaponRepository.getWeapons();
   }
