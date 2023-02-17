653552a50a91f993c1e89741eafcff8becbd9df0
moatasem <moatasemmohamed39@gmail.com>
2023-02-17 22:08:17 +0200

    finish weapons repository

---
+++ lib/features/weapons/data/repositories/weapons_repository_impl.dart
@@ -0,0 +1,51 @@
+import 'package:dartz/dartz.dart';
+import 'package:valorant_info/core/error/exceptions.dart';
+import 'package:valorant_info/core/error/failure.dart';
+import 'package:valorant_info/core/network/network_info.dart';
+import 'package:valorant_info/core/utils/app_strings.dart';
+import 'package:valorant_info/features/weapons/data/datasources/weapons_local_datasource.dart';
+import 'package:valorant_info/features/weapons/data/datasources/weapons_remote_datasource.dart';
+import 'package:valorant_info/features/weapons/domain/entities/weapon.dart';
+import 'package:valorant_info/features/weapons/domain/repositories/weapon_repository.dart';
+class WeaponRepositoryImpl implements WeaponRepository {
+  final WeaponsLocalDataSource _weaponsLocalDataSource;
+  final WeaponsRemoteDataSource _weaponsRemoteDataSource;
+  final NetworkInfo _networkInfo;
+  WeaponRepositoryImpl(
+    this._weaponsLocalDataSource,
+    this._weaponsRemoteDataSource,
+    this._networkInfo,
+  );
+  @override
+  Future<Either<Failure, List<Weapon>>> getWeapons() async{
+    List<Weapon> cachedWeapons=await _weaponsLocalDataSource.getWeaponsFromSharedPref();
+    if(await _networkInfo.isConnected)
+    {
+      if(cachedWeapons.isNotEmpty)
+      {
+        return right(cachedWeapons);
+      }
+      else
+      {
+        try{
+          final List<Weapon> result=await _weaponsRemoteDataSource.getWeapons();
+          _weaponsLocalDataSource.saveWeaponsInSharedPref(weapons: result);
+          return Right(result);
+        }on ServerException catch (failure) {
+          return Left(ServerFailure(failure.message!));
+        }
+      }
+    }
+    else
+    {
+      if(cachedWeapons.isNotEmpty)
+      {
+        return right(cachedWeapons);
+      }
+      else
+      {
+        return left(const ServerFailure(AppStrings.noInternetConnection));
+      }
+    }
+  }
+}
---
+++ lib/features/weapons/domain/repositories/weapon_repository.dart
@@ -0,0 +1,6 @@
+import 'package:dartz/dartz.dart';
+import 'package:valorant_info/core/error/failure.dart';
+import 'package:valorant_info/features/weapons/domain/entities/weapon.dart';
+abstract class WeaponRepository{
+  Future<Either<Failure,List<Weapon>>> getWeapons();
+}
